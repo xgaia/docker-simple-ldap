72929b018a3c152f70cc8c4d97ada5ffa940029b
xgaia <xgaia@gmx.com>
2020-03-30 12:05:29 +0200

    create first user once and use alpine:3.9

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,12 @@
-FROM alpine
+FROM alpine:3.9
 MAINTAINER "xavier.garnier@irisa.fr"
 
 ENV ORGANISATION_NAME "Example"
 ENV SUFFIX "dc=example,dc=com"
 ENV ROOT_USER "admin"
-ENV ROOT_PW_CLEAR "password"
+ENV ROOT_PW_CLEAR "admin"
 
-ENV FIRST_USER=true
+ENV FIRST_USER=false
 ENV USER_UID "jdoe"
 ENV USER_GIVEN_NAME "John"
 ENV USER_SURNAME "Doe"
@@ -18,9 +18,8 @@ COPY organisation.ldif /ldap/organisation.ldif
 COPY user.ldif /ldap/user.ldif
 COPY entrypoint.sh /entrypoint.sh
 
-RUN apk add --update openldap openldap-back-mdb libintl && \
-     apk add --virtual build_deps gettext && \
-     mkdir -p /run/openldap /var/lib/openldap/openldap-data /ldap /ldif && \
-     rm -rf /var/cache/apk/*
+RUN apk add --update --no-cache openldap openldap-back-mdb libintl && \
+    apk add --no-cache --virtual build_deps gettext && \
+    mkdir -p /run/openldap /var/lib/openldap/openldap-data /ldap /ldif
 
 CMD ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,23 @@
 #! /bin/sh
 
 # Create the config file
-export ROOT_PW=$(slappasswd -s "$ROOT_PW_CLEAR")
+export ROOT_PW=$(slappasswd -s "${ROOT_PW_CLEAR}")
 cat /ldap/slapd.conf | envsubst > /etc/openldap/slapd.conf
 
 # Create organisation file
 cat /ldap/organisation.ldif | envsubst > /etc/openldap/organisation.ldif
 slapadd -l /etc/openldap/organisation.ldif
 
-# create user file
-if $FIRST_USER; then
-    export USER_PW=$(slappasswd -s "$USER_PW_CLEAR")
-    cat /ldap/user.ldif | envsubst > /etc/openldap/user.ldif
-    slapadd -l /etc/openldap/user.ldif
+# Insert first user once
+test_user_file=/var/lib/openldap/openldap-data/.first_user
+
+if [ ! -f ${test_user_file} ]; then
+	if [ ${FIRST_USER} == "true" ]; then
+        touch ${test_user_file}
+        export USER_PW=$(slappasswd -s "${USER_PW_CLEAR}")
+        cat /ldap/user.ldif | envsubst > /etc/openldap/user.ldif
+        slapadd -l /etc/openldap/user.ldif
+	fi
 fi
 
 # Add *.ldif in /ldif
